1b953bd2687012639791bf84b7861b2626fc12fe
Twig-sta <camilahanguiano@gmail.com>
2026-03-09 16:12:10 -0700

    touch alpr/visualization.py

---
+++ alpr/visualization.py
@@ -0,0 +1,34 @@
+import cv2
+
+def draw_detections(frame, detections):
+    for det in detections:
+        x, y, w, h, = det['box']
+        text = det["text"]
+
+        cv2.rectangle(
+            frame,
+            (x, y),
+            (x + w, y + h),
+            (0, 255, 0),
+            2   
+        )
+
+        cv2.rectangle(
+            frame,
+            (x, y - 30),
+            (x + w, y),
+            (0, 255, 0),
+            -1  
+        )
+
+        cv2.putText(
+            frame,
+            text,
+            (x + 5, y - 5),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.6,
+            (0, 0, 0),
+            2
+        )   
+
+    return frame
